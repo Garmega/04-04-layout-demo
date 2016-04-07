3fc077a8e6eea3f2eeff4fc60798726cdab1e324
C Nicholas Mao <mao@tacktechnologies.com>
2016-04-07 12:29:47 -0700

    fdfa

--- app/src/main/java/edu/uw/layoutdemo/MainActivity.java
+++ app/src/main/java/edu/uw/layoutdemo/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uw.layoutdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LAYOUT_DEMO";
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView)findViewById(R.id.image);
+        image.setImageResource(R.drawable.android_maneki);
     }
 }
